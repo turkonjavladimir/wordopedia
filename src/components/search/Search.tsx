0b02a6ab6b1dab3c9dfe1d336f869fc5101cdd3b
turkonjavladimir <vladimir.turkonja@icloud.com>
2023-07-20 19:17:39 +0200

    updat esearch

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,37 +1,37 @@
-"use client";
+"use client"
 
-import * as z from "zod";
-import { Loader2, Search as SearchIcon } from "lucide-react";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Loader2, Search as SearchIcon } from "lucide-react"
+import { useForm } from "react-hook-form"
+import * as z from "zod"
 
-import { Input } from "@/components/ui/input";
+import useSearchURL from "@/hooks/useSearchUrl"
+import { Button } from "@/components/ui/button"
 import {
   Form,
   FormControl,
   FormField,
   FormItem,
   FormMessage,
-} from "@/components/ui/form";
-import useSearchURL from "@/hooks/useSearchUrl";
-import { Button } from "@/components/ui/button";
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
 
 const formSchema = z.object({
   query: z.string().trim().toLowerCase(),
-});
+})
 
 const Search = () => {
-  const { isPending, updateSearchURL } = useSearchURL();
+  const { isPending, updateSearchURL } = useSearchURL()
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       query: "",
     },
-  });
+  })
 
   function onSubmit(values: z.infer<typeof formSchema>) {
-    updateSearchURL(values.query);
-    form.reset();
+    updateSearchURL(values.query)
+    form.reset()
   }
 
   return (
@@ -43,7 +43,11 @@ const Search = () => {
           render={({ field }) => (
             <FormItem className="relative">
               <FormControl>
-                <Input placeholder="Search for any word..." {...field} />
+                <Input
+                  placeholder="Search for any word..."
+                  {...field}
+                  className="border-none bg-input-backgorund focus-visible:ring-1 focus-visible:ring-offset-0"
+                />
               </FormControl>
 
               <Button
@@ -53,13 +57,13 @@ const Search = () => {
                   isPending ? "opacity-0" : "opacity-100"
                 }`}
               >
-                <SearchIcon className="stroke-accent-purple h-4 w-4 cursor-pointer" />
+                <SearchIcon className="stroke-accent-purple h-4 w-4 cursor-pointer stroke-ring" />
               </Button>
 
               <Loader2
                 className={`${
                   isPending ? "opacity-100" : "opacity-0"
-                } text-muted-foreground/0.5 stroke-accent-purple absolute right-3 top-1 h-4 w-4 animate-spin`}
+                } text-muted-foreground/0.5 stroke-accent-purple absolute right-3 top-1 h-4 w-4 animate-spin stroke-ring`}
               />
               <FormMessage />
             </FormItem>
@@ -67,7 +71,7 @@ const Search = () => {
         />
       </form>
     </Form>
-  );
-};
+  )
+}
 
-export default Search;
+export default Search
