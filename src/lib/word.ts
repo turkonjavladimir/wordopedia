a6d082aa5d23edff9876f3eb02dc4f573d9470d1
turkonjavladimir <vladimir.turkonja@icloud.com>
2023-07-20 21:57:33 +0200

    fix(util): handle network errors in getWord function and update APIResponse property name

--- src/lib/word.ts
+++ src/lib/word.ts
@@ -5,23 +5,22 @@ const API_URL = "https://api.dictionaryapi.dev/api/v2/entries/en/"
 export async function getWord(word?: string): Promise<APIResponse> {
   if (!word) return {}
 
-  const res = await fetch(`${API_URL}${word}`)
+  try {
+    const res = await fetch(`${API_URL}${word}`)
+    const data = await res.json()
 
-  if (!res.ok) {
+    if (!res.ok) return { error: data, data: [] }
+
     return {
-      data: [],
-      errors: [
-        {
-          title: "Error",
-          message: "Something went wrong",
-          resolution: "Try again later",
-        },
-      ],
+      error: undefined,
+      data,
     }
-  }
-
-  return {
-    data: await res.json(),
-    errors: [],
+  } catch (error) {
+    const networkError = {
+      title: "Network Error",
+      message: "Failed to fetch data from the API",
+      resolution: "Please check your internet connection and try again.",
+    }
+    return { error: networkError, data: [] }
   }
 }
