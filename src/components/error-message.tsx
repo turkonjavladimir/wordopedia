094540f60e985d9a1136da4f0e68df586ff4c955
turkonjavladimir <vladimir.turkonja@icloud.com>
2023-07-20 21:59:18 +0200

    feat(components): Add props to ErrorMessage component
    - add title, message and resolution props to ErrorMessage

--- src/components/error-message.tsx
+++ src/components/error-message.tsx
@@ -1,17 +1,23 @@
 import { Frown } from "lucide-react"
 
-function ErrorMessage() {
+type ErrorMessage = {
+  title: string
+  message: string
+  resolution?: string
+}
+function ErrorMessage({ title, message, resolution }: ErrorMessage) {
   return (
     <div className="flex flex-col items-center">
       <Frown className="h-10 w-10 md:h-16 md:w-16" />
-      <h2 className="mb-6 mt-10 text-lg font-bold md:text-xl">
-        No Definition Found!
-      </h2>
-      <p className="text-center text-[#757575] md:text-lg">
-        Sorry pal, we couldn&apos;t find definitions for the word you were
-        looking for. You can try the search again at later time or head to the
-        web instead.
-      </p>
+      {title && (
+        <h2 className="mb-6 mt-10 text-lg font-bold md:text-xl">{title}</h2>
+      )}
+      {message && (
+        <p className="text-center text-[#757575] md:text-lg">{message}</p>
+      )}
+      {resolution && (
+        <p className="text-center text-red-600 md:text-lg">{resolution}</p>
+      )}
     </div>
   )
 }
