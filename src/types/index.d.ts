2d8d45a3eb11ef0b32d2373f1074ca2318dea3a1
turkonjavladimir <vladimir.turkonja@icloud.com>
2023-07-20 20:08:35 +0200

    feat(types): add APIResponse types

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -6,3 +6,26 @@ export type SiteConfig = {
     github: string
   }
 }
+
+type ErrorMessage = {
+  title: string
+  message: string
+  resolution: string
+}
+
+type Word = {
+  word: string
+  phonetic: string
+  phonetics: { text: string; audio: string }[]
+  meanings: { partOfSpeech: string; definitions: { definition: string }[] }[]
+  license: {
+    name: string
+    url: string
+  }
+  sourceUrls: string[]
+}
+
+export type APIResponse = {
+  data?: Result[]
+  errors?: ErrorMessage[]
+}
