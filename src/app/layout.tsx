b91a3a15c5b579394411d9e22eb91af4e10f7818
turkonjavladimir <vladimir.turkonja@icloud.com>
2023-07-20 16:37:53 +0200

    update layout, add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,58 @@
-import "./globals.css";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import "./globals.css"
 
-import { Header } from "@/components/header";
-import { ThemeProvider } from "@/components/theme-provider";
+import type { Metadata } from "next"
+import { Inter as FontSans } from "next/font/google"
 
-const inter = Inter({ subsets: ["latin"] });
+import { siteConfig } from "@/config/site"
+import { cn } from "@/lib/utils"
+import { Header } from "@/components/header"
+import { ThemeProvider } from "@/components/theme-provider"
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
 
 export const metadata: Metadata = {
-  title: "Wordopedia",
-  description:
-    "Discover the world of words with Wordopedia, your ultimate dictionary lookup app.",
-};
+  title: siteConfig.name,
+  description: siteConfig.description,
+  authors: {
+    name: "Vladimir Turkonja",
+    url: "https://github.com/turkonjavladimir",
+  },
+  creator: "Vladimir Turkonja",
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+  },
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <Header />
           {children}
         </ThemeProvider>
       </body>
     </html>
-  );
+  )
 }
