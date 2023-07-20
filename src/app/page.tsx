235d01c2335fac2d3f2a0c36d066b7f26bd7cfcb
turkonjavladimir <vladimir.turkonja@icloud.com>
2023-07-20 22:02:44 +0200

    feat(ui): display word data and error message in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,24 @@
-export default function Home() {
+import { getWord } from "@/lib/word"
+import { ErrorMessage } from "@/components/error-message"
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: { search: string }
+}) {
+  const { data, error } = await getWord(searchParams?.search)
+
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      Wordopedia
+    <main className="m-auto mt-4 max-w-3xl px-6 lg:px-0">
+      {error && <ErrorMessage title={error.title} message={error.message} />}
+
+      {data?.map((word, index: number) => {
+        return (
+          <div key={index} className="flex flex-col">
+            <h1 className="text-4xl font-bold">{word.word}</h1>
+          </div>
+        )
+      })}
     </main>
-  );
+  )
 }
