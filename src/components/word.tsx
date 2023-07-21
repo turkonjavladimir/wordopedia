ca0b04939aa4dc8573c97a6d9c0303bee60251b8
turkonjavladimir <vladimir.turkonja@icloud.com>
2023-07-21 18:30:36 +0200

    feat: implement the word component

---
+++ src/components/word.tsx
@@ -0,0 +1,64 @@
+import { Meaning } from "@/types"
+
+import { Separator } from "@/components/ui/separator"
+
+import SynonymItem from "./synonyn-item"
+
+type WordHeaderProps = {
+  word: string
+  phonetic: string
+  audioUrl: string
+}
+const WordHeader = ({ word, phonetic, audioUrl }: WordHeaderProps) => {
+  return (
+    <div className="flex items-center justify-between">
+      <div className="space-y-2 md:space-y-4">
+        {word && <p className="text-3xl font-bold md:text-6xl">{word}</p>}
+        {phonetic && (
+          <p className="text-lg text-ring md:text-2xl">{phonetic}</p>
+        )}
+      </div>
+      {audioUrl && <button>play</button>}
+    </div>
+  )
+}
+WordHeader.displaName = "WordHeader"
+
+type WordContentProps = {
+  children: React.ReactNode
+}
+
+const WordContent = ({ children }: WordContentProps) => {
+  return <div className="mt-7 space-y-7">{children}</div>
+}
+WordContent.displaName = "WordContent"
+
+type WordContentHeaderProps = {
+  label: string
+}
+const WordContentHeader = ({ label }: WordContentHeaderProps) => {
+  return (
+    <div className=" mb-7 flex items-center">
+      <span className="mr-4 text-lg font-bold italic text-[#2d2d2d] dark:text-white md:text-2xl">
+        {label}
+      </span>
+      <Separator className="flex-1" />
+    </div>
+  )
+}
+
+type WordSubContentProps = {
+  label: string
+  children: React.ReactNode
+}
+const WordSubContent = ({ label, children }: WordSubContentProps) => {
+  return (
+    <div className="mb-7">
+      <p className="mb-2 text-sm text-[#757575] underline">{label}</p>
+      {children}
+    </div>
+  )
+}
+WordSubContent.displaName = "WordSubContent"
+
+export { WordHeader, WordContent, WordContentHeader, WordSubContent }
